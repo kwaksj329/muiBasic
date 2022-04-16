4d8da9505706fe39a629cd438187a420be4a672d
kwaksj329 <kwaksj329@gmail.com>
2022-04-16 18:59:53 +0900

    adds favorite icon button

--- src/MusicList.js
+++ src/MusicList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import {Card, CardContent, Typography} from '@material-ui/core';
+import {Card, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
+import {Favorite, FavoriteBorder} from '@material-ui/icons';
 
+//styles = 함수
 const styles = theme => ({
     content : {},
     layout : {
@@ -18,10 +20,17 @@ const styles = theme => ({
 });
 
 class MusicList extends React.Component {
+
+    //id가 들어오면 함수를 return
+    toggleFavorite = (id) => () => {
+        let item = this.props.list.results.find((album) => {return album.collectionId == id})
+        item.like = true;
+    }
+
     render () {
         const {classes} = this.props;
         return (
-            <div className={classes}>
+            <div>
                 {this.props.list.results.map(item => {
                     return (
                     <Card key={item.collectionId} className={classes.card}>
@@ -29,6 +38,11 @@ class MusicList extends React.Component {
                             <Typography variant="subtitle1"> {item.artistName}</Typography>
                             <Typography variant="subtitle2"> {item.collectionCensoredName}</Typography>
                         </CardContent>
+                        <CardActions>
+                            <IconButton onClick={this.toggleFavorite(item.collectionId)}>
+                            {item.like ? <Favorite /> : <FavoriteBorder />}
+                            </IconButton>
+                        </CardActions>
                     </Card>)
                 })}
             </div>
@@ -38,3 +52,4 @@ class MusicList extends React.Component {
 }
 export default withStyles(styles)(MusicList);
 //map을 통해 list item 돌릴 때 card item에 대해 key가 있어야함 (warning 뜸)
+//this.toggleFavorite(item.collectionId) 은 함수 호출 아님! javascript의 closure와 유사함, this 꼭 붙여야함
