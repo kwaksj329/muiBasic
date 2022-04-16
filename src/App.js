0ff054d14b4f253f3d1c20bc5dc7c7c4f851274a
kwaksj329 <kwaksj329@gmail.com>
2022-04-16 16:24:10 +0900

    Added App.js and material-ui libs

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//import './App.css';
+//import PropTypes from 'prop-types';
+//import { withStyles } from '@material-ui/core/styles';
+//import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+//import AppBar from '@material-ui/core/AppBar';
+//import Toolbar from '@material-ui/core/Toolbar';
+//import MenuIcon from '@material-ui/icons/Menu';
+//import IconButton from '@material-ui/core/IconButton';
+//import ExitToApp from '@material-ui/icons/ExitToApp';
+//import Drawer from '@material-ui/core/Drawer';
+//import Forms from './Forms';
+//import HomeIcon from '@material-ui/icons/Home';
+import Typography from '@material-ui/core/Typography';
+
+//App Component
+export default class App extends React.Component {
+    render () {
+        return <Typography variant="h1">Hello World</Typography>
+    }
+}
